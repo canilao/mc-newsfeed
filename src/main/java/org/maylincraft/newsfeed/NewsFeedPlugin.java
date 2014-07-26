619b5b69c750a4d91b095dcca85192dcc8fa2133
Chris <chris.anilao@gmail.com>
2014-07-26 01:13:21 -0500

    Added functionality that will update the plugins files and directories if they are missing

--- src/main/java/org/maylincraft/newsfeed/NewsFeedPlugin.java
+++ src/main/java/org/maylincraft/newsfeed/NewsFeedPlugin.java
@@ -1,5 +1,10 @@
 package org.maylincraft.newsfeed;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.sql.SQLException;
@@ -14,12 +19,12 @@ import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.maylincraft.newsfeed.data.McmmoFullStats;
+import org.maylincraft.newsfeed.database.Database;
 import org.maylincraft.newsfeed.listeners.BlockBreakListener;
 import org.maylincraft.newsfeed.listeners.LoginListener;
 import org.maylincraft.newsfeed.listeners.McmmoXpGainListener;
 import org.maylincraft.newsfeed.listeners.PlayerDeathListener;
-import org.maylincraft.newsfeed.data.McmmoFullStats;
-import org.maylincraft.newsfeed.database.Database;
 
 ;
 
@@ -38,7 +43,7 @@ public class NewsFeedPlugin extends JavaPlugin {
       e.printStackTrace(pw);
       thePlugin.getLogger().severe(msg + " - " + sw.toString());
    }
-   
+
    public static void logWarning(String msg, Exception e) {
       StringWriter sw = new StringWriter();
       PrintWriter pw = new PrintWriter(sw);
@@ -49,6 +54,8 @@ public class NewsFeedPlugin extends JavaPlugin {
    @Override
    public void onEnable() {
       thePlugin = this;
+      // Create the directory structure.
+      createDirectoryStructure();
       // Initialize the database.
       try {
          initializeDatabase();
@@ -70,6 +77,76 @@ public class NewsFeedPlugin extends JavaPlugin {
          startWebServer();
       } catch (Exception e) {
          logSevere("Failed to initialize webserver: ", e);
+      }
+   }
+
+   private void createDirectoryStructure() {
+      boolean success = true;
+
+      File rsrcDir = new File("plugins/newsfeed/");
+      if (!rsrcDir.exists()) {
+         success &= rsrcDir.mkdir();
+      }
+      File dbDir = new File("plugins/newsfeed/db/");
+      if (!dbDir.exists()) {
+         success &= dbDir.mkdir();
+      }
+      File webDir = new File("plugins/newsfeed/web/");
+      if (!webDir.exists()) {
+         success &= webDir.mkdir();
+      }
+      webDir = new File("plugins/newsfeed/web/css/");
+      if (!webDir.exists()) {
+         success &= webDir.mkdir();
+      }
+      webDir = new File("plugins/newsfeed/web/js/");
+      if (!webDir.exists()) {
+         success &= webDir.mkdir();
+      }
+
+      try {
+         if (success) {
+            // Copy the files to the structure.
+            copyFileFromJar("/web/index.html",
+                  "plugins/newsfeed/web/index.html");
+            copyFileFromJar("/web/css/bootstrap.min.css",
+                  "plugins/newsfeed/web/css/bootstrap.min.css");
+            copyFileFromJar("/web/css/styles.css",
+                  "plugins/newsfeed/web/css/styles.css");
+            copyFileFromJar("/web/js/bootstrap.min.js",
+                  "plugins/newsfeed/web/js/bootstrap.min.js");
+            copyFileFromJar("/web/js/scripts.js",
+                  "plugins/newsfeed/web/js/scripts.js");
+         } else {
+            throw new IOException();
+         }
+      } catch (IOException e) {
+         logSevere("Failed to create directory structure", e);
+      }
+   }
+
+   private void copyFileFromJar(String resourcePath, String destinationPath)
+         throws IOException {
+      InputStream stream = NewsFeedPlugin.class
+            .getResourceAsStream(resourcePath);
+      if (stream == null) {
+         getLogger().severe("Failed to create input stream");
+      }
+      OutputStream resStreamOut = null;
+      int readBytes;
+      byte[] buffer = new byte[4096];
+      try {
+         resStreamOut = new FileOutputStream(new File(destinationPath));
+         while ((readBytes = stream.read(buffer)) > 0) {
+            resStreamOut.write(buffer, 0, readBytes);
+         }
+      } catch (IOException e) {
+         logSevere("Failed to write files", e);
+      } finally {
+         stream.close();
+         if (resStreamOut != null) {
+            resStreamOut.close();
+         }
       }
    }
 
